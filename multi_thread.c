52fe398f266a147e7cf8d584e1273fd2226b73f1
ayo-ajayi <ajayiayomide247@gmail.com>
2021-11-01 16:22:10 +0100

    formatted

--- multi_thread.c
+++ multi_thread.c
@@ -1,37 +1,41 @@
-#include <stdio.h> // Standard I/O Routines Library
+#include <stdio.h>
 #ifdef _WIN32
 #include <Windows.h>
 #else
 #include <unistd.h>
 #endif
-#include <pthread.h> // POSIX Thread Creation Library
+#include <pthread.h>
 
-
-
-void *customThreadFunction(){
-for(int i = 0; i < 15; i++){
-printf("I am a Custom Thread Function Created By Programmer.\n");
-sleep(1);
+void *customThreadFunction()
+{
+    for (int i = 0; i < 15; i++)
+    {
+        printf("I am a Custom Thread Function Created By Programmer.\n");
+        sleep(1);
+    }
+    return NULL;
 }
-return NULL;
-}
 
-int main(){
-pthread_t thread; // Thread Descriptor
-int status; // Status Variable to store the Status of the thread.
-status = pthread_create(&thread, NULL, customThreadFunction, NULL);
-/* status = 0 ==> If thread is created Sucessfully.
+int main()
+{
+    pthread_t thread;
+    int status; // Status Variable to store the Status of the thread.
+    status = pthread_create(&thread, NULL, customThreadFunction, NULL);
+    /* status = 0 ==> If thread is created Sucessfully.
 status = 1 ==> If thread is unable to create. */
-if(!status){
-printf("Custom Created Successfully.\n");
-}else{
-printf("Unable to create the Custom Thread.\n");
-return 0;
+    if (!status)
+    {
+        printf("Custom Created Successfully.\n");
+    }
+    else
+    {
+        printf("Unable to create the Custom Thread.\n");
+        return 0;
+    }
+    // Main Function For loop
+    for (int i = 0; i < 15; i++)
+    {
+        printf("I am the process thread created by compiler By default.\n");
+        sleep(1);
+    }
 }
-// Main Function For loop
-for(int i = 0; i < 15; i++){
-printf("I am the process thread created by compiler By default.\n");
-sleep(1);
-}
-}
-
